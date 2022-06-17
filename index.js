4e2e97e87d615dd3c5967af36dadce1c1cd67a11
Ruslan Rahimov <rahimoff.tr@gmail.com>
2022-06-17 14:56:00 +0300

    Added password checker

--- index.js
+++ index.js
@@ -47,34 +47,42 @@ async function mailConfirm(id) {
             console.log("Почта для подтверждения: ", colors.yellow(emailAddress));
 
             let iframe = await (await page.waitForXPath(`//iframe`)).contentFrame();
-
+            await page.waitForTimeout(1000)
             const mailChangeLink = await iframe.waitForXPath(`//div[@class="ptm"]/div/div/a`);
             await mailChangeLink.click();
-
+            await page.waitForTimeout(1000)
             const newMailInput = await iframe.waitForXPath(`//input[@name="new_email"]`);
             await newMailInput.type(emailAddress, {delay: 10});
 
             await page.waitForTimeout(2000);
             const submitFormButton = await iframe.waitForXPath(`(//button[@type="submit"])[2]`, {visible: true});
             await submitFormButton.click({delay: 10});
-
+            await page.waitForTimeout(2000)
             try {
                 const accountPasswordConfirm = await iframe.waitForXPath(`//input[@id="ajax_password"]`);
                 await accountPasswordConfirm.type(accountPassword, {delay: 10});
-
+                await page.waitForTimeout(1000)
                 const accountPasswordConfirmBtn = await iframe.waitForXPath(`//button[@data-testid="sec_ac_button"]`);
                 await accountPasswordConfirmBtn.click();
+                await page.waitForTimeout(1000)
             } catch (e) {
                 console.log('Подтверждение паролем не нужно')
             }
 
-            let closeButton = await iframe.waitForXPath(`//a[text()='Закрыть']`);
-            await closeButton.click();
-
+            try{
+                let closeButton = await iframe.waitForXPath(`//div[@class="_5lnf uiOverlayFooter _5a8u"]/a`);
+                await closeButton.click();
+            }catch (e) {
+                await iframe.waitForXPath(`//div[@id="ajax_error_msg"]`)
+                return {
+                    status: "PASSWORD ERROR", browser: browser, userData: userData,
+                };
+            }
+            await page.waitForTimeout(1000)
             console.info(colors.cyan("Иду получать код подтверждения"));
 
             const verCode = await zaush.getCode(page, emailAddress, emailPassword);
-
+            await page.waitForTimeout(1000)
             if (verCode === "NO CODE") {
                 return {
                     status: verCode, browser: browser, userData: userData,
@@ -88,16 +96,16 @@ async function mailConfirm(id) {
 
             const confirmButton = await iframe.waitForXPath(`//a[@class="_42ft _42fu SettingsEmailPendingConfirm"]`);
             await confirmButton.click();
-
+            await page.waitForTimeout(1000)
             const codeInput = await iframe.waitForXPath(`//input[@id="code"]`);
             await codeInput.type(verCode, {delay: 10});
-
+            await page.waitForTimeout(1000)
             const codeSubmitButton = await iframe.waitForXPath(`//button[text()="ОК"]`);
             await codeSubmitButton.click();
-
-            closeButton = await iframe.waitForXPath(`//a[text()='Закрыть']`);
+            await page.waitForTimeout(1000)
+            let closeButton = await iframe.waitForXPath(`//a[text()='Закрыть']`);
             await closeButton.click();
-
+            await page.waitForTimeout(1000)
             console.log(colors.greenBackground(colors.black("Почта подтверждена!")));
 
             await adspower.addAccountRemark(id, `${userData.remark}|${emailAddress}|${emailPassword}`);
@@ -167,6 +175,14 @@ const delay = async (ms) => {
                     mailConfirmResult.mailData.shift();
                     fs.writeFileSync(path.resolve(__dirname, "src", "data.txt"), mailConfirmResult.mailData.join("\r\n"));
                     await mailConfirmResult.browser.close()
+                    break
+                case 'PASSWORD ERROR':
+                    console.log(`${colors.red(`Неверный пароль аккаунта ФБ! Переименую как ${mailConfirmResult.userData.name} PASSWORD_ERROR`)}`);
+                    await adspower.accountRename(id, `${mailConfirmResult.userData.name} PASSWORD_ERROR`);
+                    idList = idList.filter((item) => item !== id);
+                    fs.writeFileSync("./src/id.txt", idList.join("\r\n"));
+                    await mailConfirmResult.browser.close()
+                    break
             }
 
         } catch (err) {
